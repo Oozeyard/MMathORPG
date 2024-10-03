fbd8b0bb2766b58845da7f7f088fa617047f3c84
Narraxxas <deurveilherjeanlouis@gmail.com>
2024-10-03 15:59:48 +0200

    Adding UI

---
+++ Scenes/CanvasLayer.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+# Références vers les ProgressBars
+@onready var health_bar = $HealthBar
+@onready var xp_bar = $XPBar
+
+# Variables pour la vie et l'XP du joueur
+var player_health = 100
+var player_max_health = 100
+var player_xp = 0
+var xp_to_next_level = 100
+var player_level = 1
+
+func _process(delta):
+	# Mettre à jour la barre de vie
+	health_bar.value = player_health
+	health_bar.max_value = player_max_health
+	
+	# Mettre à jour la barre d'XP
+	xp_bar.value = player_xp
+	xp_bar.max_value = xp_to_next_level
+
+# Fonction pour ajouter de l'XP au joueur
+func add_xp(amount):
+	player_xp += amount
+	if player_xp >= xp_to_next_level:
+		level_up()
+	# Actualiser la barre d'XP
+	xp_bar.value = player_xp
+
+# Fonction de montée de niveau
+func level_up():
+	player_level += 1
+	player_xp -= xp_to_next_level  # Conserver l'XP excédentaire
+	xp_to_next_level *= 1.5  # Augmenter la difficulté pour le niveau suivant
+	player_max_health += 20  # Augmenter la vie maximum à chaque niveau
+	player_health = player_max_health  # Restaurer la vie après une montée de niveau
+	print("Level Up! Nouveau niveau : " + str(player_level))
---
+++ Scenes/DragonCave.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var ui = $UI_Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ui.add_xp(100)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
